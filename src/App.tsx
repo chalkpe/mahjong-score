aa3eee9bd117cd7423eeb45d24ccb5151e4fe3ac
Chalk <9481405+ChalkPE@users.noreply.github.com>
2024-03-05 23:19:17 +0900

    feat: inital work

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC } from 'react'
+import { Card, Space, Statistic, Tag } from 'antd'
+import Mahgen from './components/Mahgen'
+import { HANDS } from './constants/hands'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Riichi from 'riichi'
+
+const getRandomHand = (): {
+  result: Riichi.Result
+  type: 'ron' | 'tsumo'
+  closed: string
+  taken: string
+  bakaze: string
+  jikaze: string
+} => {
+  const hand = HANDS[Math.floor(Math.random() * HANDS.length)]
+
+  const result = new Riichi(hand).calc()
+  if (!result.isAgari) return getRandomHand()
+
+  const parts = hand.split('+')
+  if (parts.length === 3) {
+    return {
+      result,
+      type: 'ron',
+      closed: parts[0],
+      taken: parts[1],
+      bakaze: parts[2][0],
+      jikaze: parts[2][1],
+    }
+  } else {
+    return {
+      result,
+      type: 'tsumo',
+      closed: parts[0].slice(0, -2),
+      taken: parts[0].slice(-2),
+      bakaze: parts[1][0],
+      jikaze: parts[1][1],
+    }
+  }
+}
+
+const App: FC = () => {
+  const { result, type, closed, taken, bakaze, jikaze } = getRandomHand()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Space direction="vertical">
+      <Card title={type === 'ron' ? '론 화료' : '쯔모 화료'}>
+        <Space align="end">
+          <Statistic title="장풍" value=" " prefix={<Mahgen sequence={bakaze + 'z'} size="small" />} />
+          <Statistic title="자풍" value=" " prefix={<Mahgen sequence={jikaze + 'z'} size="small" />} />
+          <Statistic title="대기" value=" " prefix={<Mahgen sequence={closed} size="small" />} />
+          <Statistic title="오름" value=" " prefix={<Mahgen sequence={taken} size="small" />} />
+        </Space>
+      </Card>
+      <Card>
+        <Statistic title="부판" value={result.fu} suffix={`부 ${result.han}판`} />
+
+        {Object.entries(result.yaku).map(([yaku, han]) => (
+          <Tag key={yaku}>
+            {yaku} {han}
+          </Tag>
+        ))}
+      </Card>
+    </Space>
   )
 }
 
---
+++ src/components/Mahgen.tsx
@@ -0,0 +1,51 @@
+import 'mahgen'
+import { FC, useEffect, useRef } from 'react'
+
+interface MahgenElement extends HTMLElement {
+  ['data-seq']: string
+  ['data-show-err']: string
+  ['data-river-mode']: string
+}
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'mah-gen': React.DetailedHTMLProps<
+        React.HTMLAttributes<MahgenElement>,
+        MahgenElement
+      >
+    }
+  }
+}
+
+interface MahgenProps {
+  sequence: string
+  showError?: boolean
+  riverMode?: boolean
+  size?: 'small' | 'large'
+}
+
+const Mahgen: FC<MahgenProps> = ({ sequence, showError, riverMode, size }) => {
+  const ref = useRef<MahgenElement>(null)
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.shadowRoot!.querySelector('img')!.style.height =
+        size === 'small' ? '50px' : 'unset'
+    }
+  }, [size])
+
+  return (
+    <>
+      <mah-gen
+        ref={ref}
+        data-seq={sequence.replace('||', '|')}
+        {...(showError ? { ['data-show-err']: 'true' } : {})}
+        {...(riverMode ? { ['data-river-mode']: 'true' } : {})}
+      />
+    </>
+  )
+}
+
+export default Mahgen
